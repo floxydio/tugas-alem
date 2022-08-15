16d5b1b33c298e030b8a57876b1c96b14bbcdfd6
floxydio <diooktar@gmail.com>
2022-08-15 17:18:48 +0700

    update

--- application/controllers/Mahasiswa.php
+++ application/controllers/Mahasiswa.php
@@ -6,20 +6,24 @@ class Mahasiswa extends CI_Controller {
   {
     parent::__construct();
     $this->load->model("model_mahasiswa");
+    // untuk memanggil URL di controller
     $this->load->helper('url'); 
   }
  
+  // untuk meload data mahasiswa pada DB & view mahasiswa
   public function index() {
     $this->load->model("model_mahasiswa");
     $data["results"] = $this->model_mahasiswa->getMahasiswa();
     $this->load->view("tampil_mahasiswa", $data);
   }
 
+  // fungsi untuk menampilkan form tambah mahasiswa
   public function inputmahasiswa() {
     $this->load->view("form_mahasiswa");
 
   }
-
+  
+  // fungsi untuk menambahkan data mahasiswa ke database
   public function tambahmahasiswa() {
     $this->load->model("model_mahasiswa");
     $this->model_mahasiswa->tambahMahasiswa();
@@ -27,6 +31,7 @@ class Mahasiswa extends CI_Controller {
     
   }
 
+  // fungsi untuk menghapus data mahasiswa by id
   public function delete($id) {
     $this->model_mahasiswa->deleteMahasiswa($id);
     redirect('mahasiswa');
--- application/models/model_mahasiswa.php
+++ application/models/model_mahasiswa.php
@@ -1,6 +1,8 @@
 <?php if(! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Model_Mahasiswa extends CI_Model {
+
+  // load semua data pada db
   function getMahasiswa() {
     $this->db->select('*');
     $this->db->from('uher_12190581');
@@ -8,11 +10,13 @@ class Model_Mahasiswa extends CI_Model {
     return $query->result();
   }
 
+  // fungsi untuk delete data mahasiswa
   function deleteMahasiswa($id) {
     $this->db->where('id', $id);
     $this->db->delete('uher_12190581');
   }
 
+  // fungsi untuk menambahkan data mahasiswa ke database
   function tambahMahasiswa() {
     $data = array(
       'nim' => $this->input->post('nim'),
